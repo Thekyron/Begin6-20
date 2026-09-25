8eb00dd9018cbe5dd264a3d951a21eec5c8a46c7
Thekyron <ajrapetanvladimir7@gmail.com>
2026-09-25 13:35:44 +0300

    Add files via upload

---
+++ Begin33.py
@@ -0,0 +1,6 @@
+X=float(input("сколько кг купили "))
+A=float(input("сколько заплатили "))
+Y=float(input("сколько кг нужно рассчитать "))
+kg=A/X
+total=kg*Y
+print(kg,total)
---
+++ Begin34.py
@@ -0,0 +1,8 @@
+X=float(input("сколько кг конфет купили "))
+A=float(input("сколько заплатили за конфеты "))
+Y=float(input("сколько кг ирисок купили "))
+B=float(input("сколько заплатили за ириски "))
+price_konfeti=A/X
+price_iriski=B/Y
+raznica=price_konfeti/price_iriski
+print(price_konfeti,price_iriski,raznica)
